da9deab950683409adcb29df28bad13087e5ac78
abastola <abastola@ramapo.edu>
2019-05-24 16:52:21 -0400

    Remove <-

--- main.R
+++ main.R
@@ -5,17 +5,17 @@ pacman::p_load("dplyr", "data.table", "plyr", "tidyr", "ggplot2", "knitr", "ggth
                "httr", "jsonlite", "xtable", "viridis", "kableExtra")
 
 # Api endpoints
-api_endpoints = list(reports="https://api.ers.usda.gov/data/arms/report?",
-                     categories = "https://api.ers.usda.gov/data/arms/category?",
-                     surveys="https://api.ers.usda.gov/data/arms/surveydata?",
-                     variables = "https://api.ers.usda.gov/data/arms/variable?"
+api_endpoints = list(reports    =  "https://api.ers.usda.gov/data/arms/report?",
+                     categories =  "https://api.ers.usda.gov/data/arms/category?",
+                     surveys    =  "https://api.ers.usda.gov/data/arms/surveydata?",
+                     variables  =  "https://api.ers.usda.gov/data/arms/variable?"
 )
 
 # Api key (Public API. Feel free to use the key.)
 myKey = "6RsZmKx15kBm51TuChv7rhWhZZniKf6i56db4QPt"
 
 # Filter Definition for our Survey
-filters <- list(
+filters = list(
   "year" = 2010:2017,
   "state" = "all",
   "report" = "farm Business Income Statement",
@@ -24,7 +24,7 @@ filters <- list(
 )
 
 # API POST request
-response <- POST(
+response = POST(
   url = api_endpoints[["surveys"]],
   add_headers("x-Api-key" = myKey),
   body = filters,
@@ -37,13 +37,13 @@ response <- POST(
 status_code(response)
 
 # Transform JSON response into data tables
-response_body <- content(response, "text", encoding = "UTF-8")
-result <- fromJSON(response_body)
+response_body = content(response, "text", encoding = "UTF-8")
+result = fromJSON(response_body)
 
 # Make table
-table <- as.data.frame( result[["data"]] )
-table <- jsonlite::flatten(table)
-table[,1:8 ]
+table = as.data.frame( result[["data"]] )
+table = jsonlite::flatten(table)
+table[,1:8]
 
 # Get unique category value
 unique(table[,"category_value"])
